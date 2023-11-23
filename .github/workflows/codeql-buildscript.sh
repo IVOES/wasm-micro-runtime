5c2dacf17e737ee7037ae91c1c453704ec360a89
Brian <89487381+b4yuan@users.noreply.github.com>
2023-11-22 19:28:50 -0500

    Update codeql-buildscript.sh

--- .github/workflows/codeql-buildscript.sh
+++ .github/workflows/codeql-buildscript.sh
@@ -1,30 +1,9 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2019 Intel Corporation.  All rights reserved.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+sudo apt install build-essential cmake g++-multilib libgcc-9-dev lib32gcc-9-dev ccache
 
-readonly CURRENT_PATH=$(dirname "$(realpath "$0")")
-readonly ROOT=$(realpath "${CURRENT_PATH}/..")
-
-# Change directory to the project root
-cd "${ROOT}" || exit
-
-# Remove existing build directory
-rm -rf product-mini/platforms/linux/build
-
-# Create the build directory
-mkdir -p product-mini/platforms/linux/build
-
-# Change directory to the build directory
-cd product-mini/platforms/linux/build || exit
-
-# Run CMake
+cd product-mini/platforms/linux/
+mkdir build && cd build
 cmake ..
-
-# Run Make
 make
-
-# Copy the binary to build_out directory
-echo 'Copying the binary ...'
-mkdir -p "${ROOT}/build_out"
-cp iwasm "${ROOT}/build_out/iwasm"
+# iwasm is generated under current directory
